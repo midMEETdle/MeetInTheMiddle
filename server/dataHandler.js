f018bfe037bb826d9cae18359b840a569ee41e7f
Matthew McLaughlin <Matthew McLaughlin>
2016-06-03 21:01:15 -0700

    first server commits

--- server/dataHandler.js
+++ server/dataHandler.js
@@ -1,10 +1,38 @@
 
 let dataHandler = {};
 
-dataHandler.getCoordinates = function(req, res, next) {
+dataHandler.parseInput = function(req, res, next) {
+	const googleApiKey = 'AIzaSyB9KfyHTrjZoOk7EiRzRFGqYvruh4hm6iY';
+	let url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+
+	for (let key in req.body) {
+		for (let key2 in req.body[key]) {
+			url += req.body[key][key2].replace(' ', '+');
+		}
+	}
+	url += '&key' + googleApiKey;
+
+	req.body.inputUrl = url;
+	next(); 
 };
 
 dataHandler.findCentralLocation = function(req, res, next) {
 };
+
+dataHandler.addDummyData = function (req, res, next) {
+	req.body = {};
+	req.body = {
+		address1: {
+			street: '130 Gull Street,',
+			city: 'Manhattan Beach,',
+			state: ' CA',
+		},
+		address2: {
+			street: '825 7th Street,',
+			city: 'Hermosa Beach,',
+			state: ' CA',
+		}
+	}
+}
 
 module.exports = dataHandler;
--- server/googleApiFunctions.js
+++ server/googleApiFunctions.js
@@ -3,7 +3,10 @@ const request = require('request');
 let googleApiFunctions = {};
 
 googleApiFunctions.getCoordinates = function(req, res, next) {
-	
+	request(req.body.inputUrl, function (error, response, body) {
+		console.log(body);
+		next();
+	})
 };
 
 googleApiFunctions.findCentralLocation = function(req, res, next) {
