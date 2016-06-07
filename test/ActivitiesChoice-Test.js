9413fa38b920450e0e7d5152c82becc1cf0e4cc6
Brendan Del Rosario <brendandelrosario@gmail.com>
2016-06-06 19:45:23 -0700

    add unit tests for activity component, address form component and main app component

--- test/ActivitiesChoice-Test.js
+++ test/ActivitiesChoice-Test.js
@@ -7,26 +7,26 @@ import ActivityChoice from '../client/components/activityChoices';
 
 describe('ActivityChoice Component', function() {
 
-  it('should have shit in it', function() {
-
-
-
-
+  it('should have className three columns', function() {
+    expect(shallow(<ActivityChoice />).is('.three')).to.equal(true);
   });
 
-  it('should have more shit in it', function() {
+  it('should have child tag label', function() {
+    expect(shallow(<ActivityChoice />).children('label').length).to.equal(1);
+  });
 
+  it('should have 2 child elements for label element: 1 span, 1 input', function() {
+    const labelNode = shallow(<ActivityChoice />).find('label');
+    expect(labelNode.children().length).to.equal(2);
+    expect(labelNode.children('input').length).to.equal(1);
+    expect(labelNode.children('span').length).to.equal(1);
+  });
 
-
-
-  })
-
-  it('should have even more shit in it', function() {
-
-
-
-
-  })
-
-
+  it('should have label children values equal to property value', function() {
+    const labelNode = mount(<ActivityChoice activity={'testActivity'} />).find('label');
+    const inputNode = labelNode.find('input');
+    expect(inputNode.prop('value')).to.equal('testActivity');
+    const spanNode = labelNode.find('span');
+    expect(spanNode.text()).to.equal('testActivity');
+  });
 })
--- test/App-Test.js
+++ test/App-Test.js
@@ -2,4 +2,3 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
-import Foo from '../client/components/Foo';
